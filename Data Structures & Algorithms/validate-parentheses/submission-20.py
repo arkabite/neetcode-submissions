59843f6611be160a333ef5bd1604b60ecbab462a
arkabite <56626005+arkabite@users.noreply.github.com>
2026-09-13 20:54:44 +0100

    Add: validate-parentheses - submission-20

---
+++ Data Structures & Algorithms/validate-parentheses/submission-20.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        seen={"}":"{",")":"(","]":"["}
+        stack=[]
+        for i in s:
+            if i in seen:
+                if stack and stack[-1]==seen[i]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(i)
+        
+        return not stack
